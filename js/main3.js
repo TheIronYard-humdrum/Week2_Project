9b4cc5e4ac012f0034274e506f58316448e54330
humdrum <jjrajani@gmail.com>
2016-06-18 11:06:41 -0400

    rewrote functinos.  cleaner, clearer.

---
+++ js/main3.js
@@ -0,0 +1,178 @@
+// //////////////////////////////////////////
+// ----------
+// Week 2 Project
+// Data Gathering Functions
+// -------------------
+// //////////////////////////////////////////
+
+document.write("<!DOCTYPE html><html><head><title>My First JS Generated Page</title></head><body></body></html>")
+
+var bodyNode = document.getElementsByTagName("body")
+
+// //////////////////////////////////////////
+// ------   Answer 1 ------------------------
+// ------    Return Average Price of Items --
+// ------    --------------------------------
+// ------    - itemPrice takes an array of 
+// ------       items and returns and array of
+// ------       prices
+// ------    - avg takes an array of prices
+// ------       and returns the average price.
+// //////////////////////////////////////////
+
+var itemPrice = function(items) {
+  var prices = items.map(function(item) {
+    return item.price;
+  });
+  return prices;
+};
+
+var prices = itemPrice(items);
+
+var avg = function(prices) {
+  var total = prices.reduce(function(price, next) {
+    return price + next
+  });
+  return (total/prices.length);
+};
+
+var averagePrice = avg(prices).toFixed(2);
+
+// -------------------------
+// -- Answer 1 Page Population
+// -------------------------
+// var answerOneDiv = document.createElement("DIV")
+// answerOneDiv.setAttribute("id", "answer1")
+// var answerOneHOne = document.createElement("H1")
+// var answerOneText = document.createTextNode(averagePrice);
+// answerOneHOne.appendChild(answerOneText);
+// answerOneDiv.appendChild(answerOneHOne);
+
+// //////////////////////////////////////////
+// ------   Answer 2 -------------------
+// ------    Items within Price Range --
+// ------    ------------------------------
+// ------    Takes an array of items, a min 
+// ------      price, and a max price as
+// ------      arguments.
+// //////////////////////////////////////////
+
+
+var priceRange = function(items, min, max) {
+  var inRange = items.filter(function(item) {
+    if (item.price > min && item.price < max) {
+      return item
+    }
+  });
+  return inRange;
+};
+
+var inPriceRange = priceRange(items, 14, 18);
+
+
+// //////////////////////////////////////////
+// ------   Answer 3 ----------------------
+// ------    Items Sold in GBP ------------
+// ------    ------------------------------
+// ------    Takes an array of items and a 
+// ------      currency type as arguments.
+// //////////////////////////////////////////
+
+
+var currencyType = function(items, type) {
+  var ofType = items.filter(function(item) {
+    if (item.currency_type == type) {
+      return item;
+    }
+  });
+  return ofType
+};
+
+var ofCurrencyType = currencyType(items, "GBP")
+
+// //////////////////////////////////////////
+// ------   Answer 4 ----------------------
+// ------    Display all tiems made of wood.
+// ------    ------------------------------
+// ------    Takes an array of items and 
+// ------      returns the items made of 
+// ------      the specified material.
+// //////////////////////////////////////////
+
+var materialType = function(items, material) {
+  var ofType = items.filter(function(item) {
+    if ((item.materials).includes(material)) {
+      return item
+    }
+  });
+  return ofType
+}
+
+var madeOfWood = materialType(items, "wood");
+
+// //////////////////////////////////////////
+// ------   Answer 5 ----------------------
+// ------    Display all tiems made of n+ 
+// ------     materials
+// ------    ------------------------------
+// ------    Takes an array of items and 
+// ------      returns the items made with
+// ------      n+ materials.
+// //////////////////////////////////////////
+
+var materialCount = function(items, n) {
+  var inRange = items.filter(function(item) {
+    if ((item.materials).length >= n) {
+      return item
+    }
+  });
+  return inRange;
+};
+
+var eightPlusMatierals = materialCount(items, 8);
+
+
+
+// //////////////////////////////////////////
+// ------   Answer 6 ----------------------
+// ------    Display all tiems made specified
+// ------     manufacturer.
+// ------    ------------------------------
+// ------    Takes an array of items and 
+// ------      returns the items made by
+// ------      specified manufacturer.
+// //////////////////////////////////////////
+
+var madeBy = function(items, manufacturer) {
+  var authentic = items.filter(function(item) {
+    if (item.who_made == manufacturer) {
+      return item
+    }
+  });
+  return authentic;
+};
+
+var sellerMade = madeBy(items, "i_did");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
